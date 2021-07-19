73b57bbbe634261bd4ed5e9c52050a311d517cfa
kohei-san <kohei.miyatake@gmail.com>
2021-07-19 13:50:58 +0900

    admin/family/index レイアウト調整

--- resources/views/admin/family/index.blade.php
+++ resources/views/admin/family/index.blade.php
@@ -8,10 +8,10 @@
   <div class="py-12">
     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
       <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-        <div class="p-6 bg-white border-b border-gray-200">
+        <div class="md:p-6 bg-white border-b border-gray-200">
 
           <section class="text-gray-600 body-font">
-            <div class="container px-5 mx-auto">
+            <div class="container md:px-5 mx-auto">
 
               <x-flash-message status="session('status')" />
               <div class="lg:w-2/3 w-full mx-auto overflow-auto">
@@ -21,27 +21,27 @@
                 <table class="table-auto w-full text-left whitespace-no-wrap">
                   <thead>
                     <tr>
-                      <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tl rounded-bl">名前</th>
-                      <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">メールアドレス</th>
-                      <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">作成日</th>
-                      <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100"></th>
-                      <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100"></th>
+                      <th class="md:px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tl rounded-bl">名前</th>
+                      <th class="md:px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">メールアドレス</th>
+                      <th class="md:px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">作成日</th>
+                      <th class="md:px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100"></th>
+                      <th class="md:px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100"></th>
                     </tr>
                   </thead>
                   <tbody>
                     @foreach($families as $family)
                       <tr>
-                        <td class="px-4 py-3">{{ $family->name }}</td>
-                        <td class="px-4 py-3">{{ $family->email }}</td>
-                        <td class="px-4 py-3">{{ $family->created_at->diffForHumans() }}</td>
+                        <td class="md:px-4 py-3">{{ $family->name }}</td>
+                        <td class="md:px-4 py-3">{{ $family->email }}</td>
+                        <td class="md:px-4 py-3">{{ $family->created_at->diffForHumans() }}</td>
                         <td>
-                          <button onclick="location.href='{{ route('admin.family.edit', ['family'=> $family->id]) }}'" class="text-white bg-gray-300 border-0 py-2 px-4 focus:outline-none hover:bg-indigo-600 rounded">編集</button>
+                          <button onclick="location.href='{{ route('admin.family.edit', ['family'=> $family->id]) }}'" class="text-white bg-gray-300 border-0 py-2 md:px-4 focus:outline-none hover:bg-indigo-600 rounded">編集</button>
                         </td>
                         <form id="delete_{{ $family->id }}" action="{{ route('admin.family.destroy', ['family' => $family->id]) }}" method="post">
                           @csrf
                           @method('delete')
                           <td>
-                            <a href="#" onclick="deletePost(this)" data-id="{{ $family->id }}" class="text-white bg-gray-300 border-0 py-2 px-4 focus:outline-none hover:bg-red-600 rounded">削除</a>
+                            <a href="#" onclick="deletePost(this)" data-id="{{ $family->id }}" class="text-white bg-gray-300 border-0 py-2 md:px-4 focus:outline-none hover:bg-red-600 rounded">削除</a>
                           </td>
                         </form>
                       </tr>
