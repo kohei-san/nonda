89feeff4a1766e5ad563839ce88f608f86ab4bdc
kohei-san <kohei.miyatake@gmail.com>
2021-07-31 21:33:52 +0900

    user:トップ画面作成

--- resources/views/user/dashboard.blade.php
+++ resources/views/user/dashboard.blade.php
@@ -4,16 +4,16 @@
             こんにちは
         </h2>
     </x-slot>
-
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-                    <img src="{{ Storage::url('public/storage/'.$image->image) }}" alt="" class="w-10 h-10">
-                </div>
+    
+    <section class="text-gray-600 body-font">
+        <div class="container px-5 py-24 mx-auto flex flex-wrap flex-col">
+            <img class="xl:w-1/2 lg:w-1/2 md:w-3/4 w-full block mx-auto mb-10 object-cover object-center rounded" alt="hero" src="{{ Storage::url('public/storage/'. $image->image) }}">
+            <div class="flex flex-col text-center w-full">
+                <h1 class="text-xl font-medium title-font mb-4 text-gray-900">{{ $image->title }}</h1>
+                <p class="lg:w-2/3 mx-auto leading-relaxed text-base">{{ $image->message }}</p>
             </div>
         </div>
-        <x-user-button></x-user-button>
-    </div>
+    </section>
+    <x-user-button></x-user-button>
 </x-app-layout>
 
