8762cd4d2ebf07f14d47caf35fb8d3fcd10e14af
kohei-san <kohei.miyatake@gmail.com>
2021-08-03 10:30:30 +0900

    テーマカラー適応、フォント調整

--- resources/views/user/button.blade.php
+++ resources/views/user/button.blade.php
@@ -4,59 +4,59 @@
   </x-slot>
     <section class="text-gray-600 body-font">
       <div class="container px-5 py-24 mx-auto">
-        <div class="flex flex-wrap lg:w-4/5 sm:mx-auto sm:mb-2 -mx-2">
+        <div class="flex flex-wrap content-around lg:w-4/5 sm:mx-auto sm:mb-2 -mx-2">
           <div class="p-2 sm:w-1/2 w-full">
             <div class="bg-gray-100 rounded flex p-4 h-full items-center">
-              <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" class="text-red-400 w-6 h-6 flex-shrink-0 mr-4" viewBox="0 0 24 24">
+              <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" class="text-theme w-6 h-6 flex-shrink-0 mr-4" viewBox="0 0 24 24">
                 <path d="M22 11.08V12a10 10 0 11-5.93-9.14"></path>
                 <path d="M22 4L12 14.01l-3-3"></path>
               </svg>
-              <span class="title-font font-medium">Authentic Cliche Forage</span>
+              <span class="title-font font-medium">薬_1</span>
             </div>
           </div>
           <div class="p-2 sm:w-1/2 w-full">
             <div class="bg-gray-100 rounded flex p-4 h-full items-center">
-              <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" class="text-red-400 w-6 h-6 flex-shrink-0 mr-4" viewBox="0 0 24 24">
+              <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" class="text-theme w-6 h-6 flex-shrink-0 mr-4" viewBox="0 0 24 24">
                 <path d="M22 11.08V12a10 10 0 11-5.93-9.14"></path>
                 <path d="M22 4L12 14.01l-3-3"></path>
               </svg>
-              <span class="title-font font-medium">Kinfolk Chips Snackwave</span>
+              <span class="title-font font-medium">薬_2</span>
             </div>
           </div>
           <div class="p-2 sm:w-1/2 w-full">
             <div class="bg-gray-100 rounded flex p-4 h-full items-center">
-              <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" class="text-red-400 w-6 h-6 flex-shrink-0 mr-4" viewBox="0 0 24 24">
+              <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" class="text-theme w-6 h-6 flex-shrink-0 mr-4" viewBox="0 0 24 24">
                 <path d="M22 11.08V12a10 10 0 11-5.93-9.14"></path>
                 <path d="M22 4L12 14.01l-3-3"></path>
               </svg>
-              <span class="title-font font-medium">Coloring Book Ethical</span>
+              <span class="title-font font-medium">薬_3</span>
             </div>
           </div>
           <div class="p-2 sm:w-1/2 w-full">
             <div class="bg-gray-100 rounded flex p-4 h-full items-center">
-              <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" class="text-red-400 w-6 h-6 flex-shrink-0 mr-4" viewBox="0 0 24 24">
+              <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" class="text-theme w-6 h-6 flex-shrink-0 mr-4" viewBox="0 0 24 24">
                 <path d="M22 11.08V12a10 10 0 11-5.93-9.14"></path>
                 <path d="M22 4L12 14.01l-3-3"></path>
               </svg>
-              <span class="title-font font-medium">Typewriter Polaroid Cray</span>
+              <span class="title-font font-medium">サプリ_A</span>
             </div>
           </div>
           <div class="p-2 sm:w-1/2 w-full">
             <div class="bg-gray-100 rounded flex p-4 h-full items-center">
-              <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" class="text-red-400 w-6 h-6 flex-shrink-0 mr-4" viewBox="0 0 24 24">
+              <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" class="text-theme w-6 h-6 flex-shrink-0 mr-4" viewBox="0 0 24 24">
                 <path d="M22 11.08V12a10 10 0 11-5.93-9.14"></path>
                 <path d="M22 4L12 14.01l-3-3"></path>
               </svg>
-              <span class="title-font font-medium">Pack Truffaut Blue</span>
+              <span class="title-font font-medium">サプリ_B</span>
             </div>
           </div>
           <div class="p-2 sm:w-1/2 w-full">
             <div class="bg-gray-100 rounded flex p-4 h-full items-center">
-              <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" class="text-red-400 w-6 h-6 flex-shrink-0 mr-4" viewBox="0 0 24 24">
+              <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" class="text-theme w-6 h-6 flex-shrink-0 mr-4" viewBox="0 0 24 24">
                 <path d="M22 11.08V12a10 10 0 11-5.93-9.14"></path>
                 <path d="M22 4L12 14.01l-3-3"></path>
               </svg>
-              <span class="title-font font-medium">The Catcher In The Rye</span>
+              <span class="title-font font-medium">サプリ_C</span>
             </div>
           </div>
         </div>
--- resources/views/user/dashboard.blade.php
+++ resources/views/user/dashboard.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            こんにちは
+        <h2 class="font-medium text-xl text-gray-800 leading-tight">
+            こんにちはnondaさん
         </h2>
     </x-slot>
     
@@ -9,8 +9,8 @@
         <div class="container px-5 py-24 mx-auto flex flex-wrap flex-col">
             <img class="xl:w-1/2 lg:w-1/2 md:w-3/4 w-full block mx-auto mb-10 object-cover object-center rounded" alt="hero" src="{{ Storage::url('public/storage/'. $image->image) }}">
             <div class="flex flex-col text-center w-full">
-                <h1 class="text-xl font-medium title-font mb-4 text-gray-900">{{ $image->title }}</h1>
-                <p class="lg:w-2/3 mx-auto leading-relaxed text-base">{{ $image->message }}</p>
+                <h1 class="text-2xl font-semibold mb-4 text-gray-900">{{ $image->title }}</h1>
+                <p class="lg:w-2/3 mx-auto leading-relaxed text-lg">{{ $image->message }}</p>
             </div>
         </div>
     </section>
